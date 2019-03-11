87027b55021cc4617dd80f3a78b98b8fa4d2009c
mpeutz <mike.peutz@dynamicragelabs.com>
2019-03-10 17:43:24 -0700

    Add color parameter to shadow

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
 const postcss = require('postcss');
 const valueParser = require('postcss-value-parser');
+const tinycolor = require('tinycolor2');
 
 module.exports = postcss.plugin('shadow', () => {
 
-    
+
     return function (css) {
         css.walkRules(rule => {
-            
+
             rule.walkDecls(decl => {
 
-                function shadows(depth) {
+                function shadows(depth, hue) {
 
                     if (depth < 0 || depth > 25) {
                         result.warn('shadow depth is out of range. [0-25]');
                     }
-                
+
                     const alpha = (num, in_min, in_max, out_min, out_max) => {
                         return Number(((num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min).toFixed(2));
                     }
-                
-                    return `0px ${Math.ceil(depth * .458333)}px ${Math.ceil(depth * .625)}px -${Math.ceil(depth * .29166)}px rgba(0, 0, 0, ${alpha(depth,0,25,0.1,0.35)}), 0px ${Math.ceil(depth)}px ${Math.ceil(depth * 1.5833)}px ${Math.ceil(depth * .125)}px rgba(0, 0, 0, ${alpha(depth,0,25,0.075,0.25)}), 0px ${Math.ceil(depth * .375)}px ${Math.ceil(depth * 1.916)}px ${Math.ceil(depth * .3333)}px rgba(0, 0, 0, ${alpha(depth,0,25,0.05,0.15)})`;
-                
+
+                    return `0px ${Math.ceil(depth * .458333)}px ${Math.ceil(depth * .625)}px -${Math.ceil(depth * .29166)}px ${tinycolor(hue).setAlpha(alpha(depth,0,25,0.1,0.35)).toRgbString()}, 0px ${Math.ceil(depth)}px ${Math.ceil(depth * 1.5833)}px ${Math.ceil(depth * .125)}px ${tinycolor(hue).setAlpha(alpha(depth,0,25,0.075,0.25)).toRgbString()}, 0px ${Math.ceil(depth * .375)}px ${Math.ceil(depth * 1.916)}px ${Math.ceil(depth * .3333)}px ${tinycolor(hue).setAlpha(alpha(depth,0,25,0.05,0.15)).toRgbString()}`;
+
+                }
+
+                function parseParams(val) {
+
+                    let shadowParams = [];
+                    
+
+                    if (val.trim().indexOf(' ') != -1) {
+                        shadowParams = val
+                            .match(/^(\S+)\s(.*)/).slice(1)
+                            .map(str => str.trim());
+                    } else {
+                        shadowParams = [val, '#000'];
+                    }
+
+                    let dpth = parseInt(shadowParams[0]);
+                    let col = shadowParams[1];
+
+                    return [dpth, col]
                 }
 
 
                 if (decl.value.includes('shadow')) {
 
                     const parsedValue = valueParser(decl.value)
-                    parsedValue.walk(node => {
-                        
-                        if (node.value === 'shadow') {
-                            
-                            let val = parseInt(node.nodes[0].value);
-                
-                            node.value = shadows(val);
 
-                            decl.value = parsedValue.toString().replace(`(${val})`, '');
+                    parsedValue.walk(node => {
+
+
+                        if (node.value === 'shadow') {
+
+
+                            let shade = parseParams(valueParser.stringify(node.nodes));
+                            let rep = valueParser.stringify(node).replace('shadow', '');
+
+                            node.value = shadows(shade[0], shade[1]);
+
+                            decl.value = parsedValue.toString().replace(`${rep}`, '');
                         }
                     })
 
@@ -43,20 +67,19 @@ module.exports = postcss.plugin('shadow', () => {
 
                 if (decl.prop == "shadow") {
 
-                    let shade = shadows(decl.value);
-    
-    
+                    let shade = parseParams(decl.value);
+
                     decl.cloneBefore({
                         prop: 'box-shadow',
-                        value: shade
+                        value: shadows(shade[0], shade[1])
                     });
-    
+
                     decl.remove();
                 }
 
 
             })
         })
-        
+
     };
 });
--- test/shadow.test.js
+++ test/shadow.test.js
@@ -55,15 +55,22 @@ test('handles shadows depth 25', t => {
 
 test('handles inline shadows depth 25', t => {
   return run(t,
-    '.shadow { box-shadow: 0 0 8px #000, shadow(25), 0 0 8px #fff; }',
+    '.shadow { box-shadow: 0 0 8px #000, shadow( 25), 0 0 8px #fff; }',
     '.shadow { box-shadow: 0 0 8px #000, 0px 12px 16px -8px rgba(0, 0, 0, 0.35), 0px 25px 40px 4px rgba(0, 0, 0, 0.25), 0px 10px 48px 9px rgba(0, 0, 0, 0.15), 0 0 8px #fff; }'
   );
 });
 
-test('handles inline shadows depth 15 plus bevel', t => {
-  return run(t,
-    '.shadow { box-shadow: inset 0 1px 1px #000, shadow(15), inset 0 -1px 1px #fff; }',
-    '.shadow { box-shadow: inset 0 1px 1px #000, 0px 7px 10px -5px rgba(0, 0, 0, 0.25), 0px 15px 24px 2px rgba(0, 0, 0, 0.18), 0px 6px 29px 5px rgba(0, 0, 0, 0.11), inset 0 -1px 1px #fff; }'
-  );
+
+test('handles shadows depth 5 with color', t => {
+    return run(t,
+      '.shadow { shadow: 5 #00ddff; }',
+      '.shadow { box-shadow: 0px 3px 4px -2px rgba(0, 221, 255, 0.15), 0px 5px 8px 1px rgba(0, 221, 255, 0.11), 0px 2px 10px 2px rgba(0, 221, 255, 0.07); }'
+    );
 });
 
+test('Handles inline shadow with color', t => {
+  return run(t,
+    '.shadow { box-shadow: shadow(15 #f00); }',
+    '.shadow { box-shadow: 0px 7px 10px -5px rgba(255, 0, 0, 0.25), 0px 15px 24px 2px rgba(255, 0, 0, 0.18), 0px 6px 29px 5px rgba(255, 0, 0, 0.11); }'
+  );
+});
